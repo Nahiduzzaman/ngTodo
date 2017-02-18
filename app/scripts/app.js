ca0a43f70160632ec4de4cd620862092e622a4d8
Md. Nahiduzzaman Rose <Md. Nahiduzzaman Rose>
2017-02-18 18:48:10 +0600

    bowl function

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -14,23 +14,132 @@ angular
     'ngAria',
     'ngCookies',
     'ngResource',
-    'ngRoute',
     'ngSanitize',
     'ngTouch',
     'ui.router'
   ])
-  .config(function ($stateProvider, $urlRouterProvider) {
+  .config(function ($stateProvider, $urlRouterProvider,$locationProvider) {
     $urlRouterProvider.otherwise('/');
     $stateProvider
-      .state('main', {
-            url: '/',
-            templateUrl: 'views/main.html',
-            controller: 'MainCtrl'
+      .state('start', {
+        url: '/',
+        templateUrl: 'views/start.html',
+        controller: 'StartCtrl'
       })
-      .state('about', {
-        url: '/about',
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl',
+      .state('play', {
+        url: '/play',
+        templateUrl: 'views/play.html',
+        controller: 'PlayCtrl'
+      })
+      .state('play-history', {
+        url: '/play/:over/:ball',
+        templateUrl: 'views/play-history.html',
+        controller: 'PlayCtrl'
       });
+
+    $locationProvider.hashPrefix('');
       
+  })
+  .service('StatisticService', function($filter) {
+   /*var number_of_ball = 0;
+   var wicket = 0;
+   var run = 0;
+   var total_run = 0;
+   var score = 0;
+   var over = 0;*/
+   var scoreArray = [0,1,2,3,4,6,'W','WD','NB'];
+   //var statistics = [];
+   
+   if(JSON.parse(localStorage.getItem("gameData")) == null){
+      var statistics = [];
+      var number_of_ball = 0;
+      var wicket = 0;
+      var run = 0;
+      var total_run = 0;
+      var score = 0;
+      var over = 0;
+   }
+   else{
+      var statistics = JSON.parse(localStorage.getItem("gameData"));
+      var number_of_ball = statistics[statistics.length-1].ball;
+      var wicket = statistics[statistics.length-1].wicket;
+      var run = statistics[statistics.length-1].run_per_ball;
+      var total_run = statistics[statistics.length-1].total_run;
+      var score = statistics[statistics.length-1].score;
+      var over = statistics[statistics.length-1].over; 
+   }
+
+   var gameData = {
+      ball: null,
+      over: null,
+      score: null,
+      run_per_ball: null,
+      total_run: null,
+      wicket: null
+   };
+
+
+   return {
+      play: function(){
+         number_of_ball++;
+     
+         score = scoreArray[Math.floor(Math.random() * scoreArray.length)];
+         if(number_of_ball == 6){
+            number_of_ball = 0;
+            over++;
+         }
+
+         
+         //console.log('initial','Overs: '+over+'.'+number_of_ball,'Score: '+score);
+         switch (score) {
+            case 'WD':
+               run = 1;
+               number_of_ball = number_of_ball - 1;
+               break;
+            case 'NB':
+               run = 1;
+               number_of_ball = number_of_ball - 1;
+               break;
+            case 'W':
+               run = 0;
+               wicket = wicket + 1;
+               break;
+            default: 
+               run = score;
+         }
+         total_run = total_run + run;
+
+         console.log('final','Overs: '+over+'.'+number_of_ball,'Score: '+score, 'RPB: '+run, 'Total: '+total_run,'Wicket:'+wicket);
+
+         /*if(number_of_ball < 0){
+            number_of_ball = number_of_ball + 1;
+         }*/
+         console.log(number_of_ball)
+         gameData = {
+            ball: number_of_ball<0 ? 5:number_of_ball,
+            over: number_of_ball<0 ? ((over-1)<1 ? 0:over):over,
+            score: score,
+            run_per_ball: run,
+            total_run: total_run,
+            wicket: wicket
+         }
+         statistics.push(gameData);
+         console.log('statistics',statistics);
+         return statistics;
+      },
+
+      get: function(ball,overs,data) {
+        console.log('ball',ball);
+        console.log('overs',overs);
+        console.log('data',data);
+        if(data){
+           var scoresbyball = $filter('filter')(data, {'ball':ball,'over':over});
+           console.log('scoresbyballArray',scoresbyball);
+           var indexOfScore = data.indexOf(scoresbyball[0]);
+           scoresbyball[0].indexOfScore = indexOfScore;
+           console.log('scoresbyball',scoresbyball[0]);
+           return scoresbyball[0];
+        }
+      }
+    }
   });
---
+++ app/scripts/controllers/play.js
@@ -0,0 +1,32 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name todoApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the todoApp
+ */
+angular.module('todoApp')
+  .controller('PlayCtrl', function ($stateParams,StatisticService,$scope) {
+  	
+  	$scope.teamData = JSON.parse(localStorage.getItem("teamData"));
+  	console.log('teamData',$scope.teamData);
+
+  	$scope.bowl = function(){
+  		var gameData = StatisticService.play();
+  		localStorage.setItem("gameData", JSON.stringify(gameData));
+  		$scope.gameData = JSON.parse(localStorage.getItem("gameData"));
+  		console.log($scope.gameData);
+  	}
+
+  	$scope.gameData = JSON.parse(localStorage.getItem("gameData"));
+
+  	
+
+
+
+	$scope.getStats = StatisticService.get($stateParams.ball,$stateParams.over,$scope.gameData);
+	console.log('getStats',$scope.getStats);
+  	    
+  });
